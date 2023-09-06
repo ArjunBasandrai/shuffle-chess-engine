d6460dbe8e633726100fb35f5a040fd38f557008
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-09-06 22:08:59 +0530

    feat: masking relevant rook occupancy bits for generating key for magic bitboards

--- chess.c
+++ chess.c
@@ -120,6 +120,21 @@ U64 mask_bishop_attacks(int square){
     return attacks;
 }
 
+U64 mask_rook_attacks(int square){
+    U64 attacks = 0ULL;
+
+    int r, f;
+    int tr = square / 8;
+    int tf = square % 8;
+
+    for (r = tr + 1; r <= 6; r++) attacks |= (1ULL << (r * 8 + tf));
+    for (r = tr - 1; r >= 1; r--) attacks |= (1ULL << (r * 8 + tf));
+    for (f = tf + 1; f <= 6; f++) attacks |= (1ULL << (tr * 8 + f));
+    for (f = tf - 1; f >= 1; f--) attacks |= (1ULL << (tr * 8 + f));
+
+    return attacks;
+}
+
 void init_leaper_attacks() {
     for(int square = 0; square < 64; square++){
         pawn_attacks[white][square] = mask_pawn_attacks(square,white);
@@ -135,8 +150,8 @@ void init_leaper_attacks() {
 int main(){
     init_leaper_attacks();
     for (int square = 0; square < 64; square++){
-        print_bitboard(mask_bishop_attacks(square));
+        print_bitboard(mask_rook_attacks(square));
     }
-    // print_bitboard(mask_bishop_attacks(a8));
+    // print_bitboard(mask_rook_attacks(d4));
     return 0;
 }
