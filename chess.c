eaa1c8698cae91aed203d39dbf2bb2bcb29200f6
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-09-10 23:00:16 +0530

    chore: moved movegen code to separate header

--- chess.c
+++ chess.c
@@ -41,6 +41,8 @@
 #endif
 
 #include "helpers/fen.h"
+#include "helpers/movegen.h"
+
 
 #ifndef U64
 #define U64 unsigned long long
@@ -53,43 +55,7 @@ void init_all() {
     // init_magic_numbers();
 }
 
-static inline int is_square_attacked(int square, int side) {
 
-    if ((side == white) && (pawn_attacks[black][square] & bitboards[P])) return 1;
-
-    if ((side == black) && (pawn_attacks[white][square] & bitboards[p])) return 1;
-
-    if (knight_attacks[square] & ((side == white) ? bitboards[N] : bitboards[n])) return 1;
-
-    if (get_bishop_attacks(square,occupancies[both]) & ((side == white) ? bitboards[B] : bitboards[b])) return 1;
-
-    if (get_rook_attacks(square,occupancies[both]) & ((side == white) ? bitboards[R] : bitboards[r])) return 1;
-
-    if (get_queen_attacks(square,occupancies[both]) & ((side == white) ? bitboards[Q] : bitboards[q])) return 1;
-
-
-    if (king_attacks[square] & ((side == white) ? bitboards[K] : bitboards[k])) return 1;
-
-    return 0;
-}
-
-void print_attacked_squares(int side) {
-    printf("\n");
-    for (int rank = 0; rank < 8; rank++) {
-        for (int file = 0; file < 8; file++) {
-            int square = rank * 8 + file;
-
-            if (!file) {
-                printf("%d  ", 8 - rank);
-            }
-            printf("%d ",is_square_attacked(square, side) ? 1 : 0);
-        }
-        printf("\n");
-    }
-
-    printf("\n   a b c d e f g h\n\n");
-
-}
 
 // Main driver
 int main(){
---
+++ helpers/movegen.h
@@ -0,0 +1,57 @@
+#ifndef STDIO_H_
+#define STDIO_H_
+#include <stdio.h>
+#endif
+
+#ifndef CONST_H_
+#define CONST_H_
+#include "helpers/board_constants.h"
+#endif
+
+#ifndef PCTABLES_H_
+#define PCTABLES_H_
+#include "helpers/pre_calculated_tables.h"
+#endif
+
+#ifndef BOARD_H_
+#define BOARD_H_
+#include "helpers/board.h"
+#endif
+
+static inline int is_square_attacked(int square, int side) {
+
+    if ((side == white) && (pawn_attacks[black][square] & bitboards[P])) return 1;
+
+    if ((side == black) && (pawn_attacks[white][square] & bitboards[p])) return 1;
+
+    if (knight_attacks[square] & ((side == white) ? bitboards[N] : bitboards[n])) return 1;
+
+    if (get_bishop_attacks(square,occupancies[both]) & ((side == white) ? bitboards[B] : bitboards[b])) return 1;
+
+    if (get_rook_attacks(square,occupancies[both]) & ((side == white) ? bitboards[R] : bitboards[r])) return 1;
+
+    if (get_queen_attacks(square,occupancies[both]) & ((side == white) ? bitboards[Q] : bitboards[q])) return 1;
+
+
+    if (king_attacks[square] & ((side == white) ? bitboards[K] : bitboards[k])) return 1;
+
+    return 0;
+}
+
+void print_attacked_squares(int side) {
+    printf("\n");
+    for (int rank = 0; rank < 8; rank++) {
+        for (int file = 0; file < 8; file++) {
+            int square = rank * 8 + file;
+
+            if (!file) {
+                printf("%d  ", 8 - rank);
+            }
+            printf("%d ",is_square_attacked(square, side) ? 1 : 0);
+        }
+        printf("\n");
+    }
+
+    printf("\n   a b c d e f g h\n\n");
+
+}
