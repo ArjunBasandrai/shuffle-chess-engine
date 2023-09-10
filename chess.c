43dcc93bff4fd49103a7521aef36008dbe8b2871
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-09-10 21:40:58 +0530

    feat: added is_square_attacked function
    - Added function to check if a square is attacked
    - Doesn't yet take into account if the attack is by friendly piece or not

--- chess.c
+++ chess.c
@@ -53,21 +53,51 @@ void init_all() {
     // init_magic_numbers();
 }
 
+static inline int is_square_attacked(int square, int side) {
+
+    if ((side == white) && (pawn_attacks[black][square] & bitboards[P])) return 1;
+
+    if ((side == black) && (pawn_attacks[white][square] & bitboards[p])) return 1;
+
+    if (knight_attacks[square] & ((side == white) ? bitboards[N] : bitboards[n])) return 1;
+
+    if (get_bishop_attacks(square,occupancies[both]) & ((side == white) ? bitboards[B] : bitboards[b])) return 1;
+
+    if (get_rook_attacks(square,occupancies[both]) & ((side == white) ? bitboards[R] : bitboards[r])) return 1;
+
+    if (get_queen_attacks(square,occupancies[both]) & ((side == white) ? bitboards[Q] : bitboards[q])) return 1;
+
+
+    if (king_attacks[square] & ((side == white) ? bitboards[K] : bitboards[k])) return 1;
+
+    return 0;
+}
+
+void print_attacked_sqaures(int side) {
+    printf("\n");
+    for (int rank = 0; rank < 8; rank++) {
+        for (int file = 0; file < 8; file++) {
+            int square = rank * 8 + file;
+
+            if (!file) {
+                printf("%d  ", 8 - rank);
+            }
+            printf("%d ",is_square_attacked(square, side) ? 1 : 0);
+        }
+        printf("\n");
+    }
+
+    printf("\n   a b c d e f g h\n\n");
+
+}
+
 // Main driver
 int main(){
     init_all();
 
-    U64 occupancy = 0ULL;
-    set_bit(occupancy,b6);
-    set_bit(occupancy,d6);
-    set_bit(occupancy,f6);
-    set_bit(occupancy,b4);
-    set_bit(occupancy,g4);
-    set_bit(occupancy,c3);
-    set_bit(occupancy,d3);
-    set_bit(occupancy,e3);
-
-    print_bitboard(get_queen_attacks(d4,occupancy));
+    parse_fen(tricky_position);
+    print_board();
+    print_attacked_sqaures(white);
 
     return 0;
 }
