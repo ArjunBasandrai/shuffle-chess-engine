b7eb5be23f6ad5209744d9a26a28fa0edbc4bad6
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-10-16 21:39:57 +0530

    feat: added positional scoring using optimized interpolation

--- shuffle.c
+++ shuffle.c
@@ -95,7 +95,7 @@ void init_all() {
 int main(){
     init_all();
 
-    int debug = 1;
+    int debug = 0;
 
     if (debug) {
         parse_fen("r3k2r/p1ppqpb1/1n2pnp1/3PN3/1p2P3/2N2Q1p/PPPB1PPP/R3K2R w KQkq - 0 1 ");
